c080f68b1a52c5efb5ed5dc82ccfb6dcae44b212
pro7357 <pro7357@protonmail.com>
2023-04-15 18:09:11 +0800

    Fix bug when sending all chats to terminal

--- extension/popup.js
+++ extension/popup.js
@@ -22,7 +22,7 @@ document.getElementById("test-btn").addEventListener("click", () => {
 // Function to get chats in the page
 function getChats() {
   // Get the chat container element and extract the chat text
-  const chatContainer = document.querySelector('div.overflow-hidden.w-full.h-full.relative div.flex.h-full.flex-1.flex-col.md\\:pl-\\[260px\\] main div');
+  const chatContainer = document.querySelector('div.overflow-hidden.w-full.h-full.relative main div');
   const chatText = chatContainer.innerText;
 
   // Split the chat text into an array and return it
